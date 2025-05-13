1fbb625d6dd5ba7872fead841f23820aea1f9c5c
Akshay Vijayvergiya <akvv2596@gmail.com>
2025-05-13 15:23:29 -0400

    Problem-1.java updated with comments

--- Problem-1.java
+++ Problem-1.java
@@ -1,3 +1,25 @@
+/*
+ * Uses an array of lists (buckets) to store keys with chaining for collision handling.
+ * 1. buckets: an array of ArrayLists, each holding integers that hash to the same index.
+ * 2. PRIME: a prime number used for modulo to distribute keys evenly.
+
+ * - hash(key): computes index using a simple hash function.
+ * - add: places key in its bucket if not already present.
+ * - remove: deletes key from its bucket if it exists.
+ * - contains: checks if key exists in its computed bucket.
+ */
+
+/*
+* Another approach I thought was to take an boolean array[1001][1001] that can easily accomodate the input range till 10^6
+* and mark true for the [key/1001][key%1001] which will eventually be unique for every unique key
+But this approach is not properly hashing but it is open addressing
+*/
+
+// Time Complexity : O(1) average, O(n) worst-case (if all keys hash to the same bucket) for all functions
+// Space Complexity : O(n), where n is the number of keys stored.
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No (Explored multiple ways)
+
 class MyHashSet {
 
     int PRIME = 997;
